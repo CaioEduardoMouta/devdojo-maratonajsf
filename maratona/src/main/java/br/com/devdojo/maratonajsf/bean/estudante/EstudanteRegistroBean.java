f2944cae9493cab7f993c791079726c65adde139
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-07-04 20:23:53 -0300

    JSF

--- maratona/src/main/java/br/com/devdojo/maratonajsf/bean/estudante/EstudanteRegistroBean.java
+++ maratona/src/main/java/br/com/devdojo/maratonajsf/bean/estudante/EstudanteRegistroBean.java
@@ -2,10 +2,13 @@ package br.com.devdojo.maratonajsf.bean.estudante;
 
 import java.io.Serializable;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import br.com.devdojo.maratonajsf.model.Estudante;
@@ -14,6 +17,7 @@ import java.io.Serializable;
 
 
 @Named
+@ViewScoped
 public class EstudanteRegistroBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -22,10 +26,68 @@ public class EstudanteRegistroBean implements Serializable {
 	private String[] nomeArray = {"Caio", "e", "Lindo"};
 	private List<String> nomeList = Arrays.asList("Chelsea FC", "Manchester", "Arsenal", "Liverpool");
 	private Set<String> nomeSet = new HashSet<>(Arrays.asList("Portugal", "Angola", "Cabo Verde", "Brasil"));
+	private Map<String,String> nomeMap = new HashMap<>();
+	private boolean mostrarNota;
+	private boolean mostrarLink;
+	{	
+		nomeMap.put("Calistenia", "Barra");
+		nomeMap.put("Cross Fit", "Resistencia");
+		nomeMap.put("StrongMan", "Força");
+//		
+//		for (Map.Entry<String, String> entry : nomeMap.entrySet()) {
+//			System.out.println(entry.getKey());
+//			System.out.println(entry.getValue());
+//		}
+	}
+	
+	public void executar() {
+		System.out.println("Fazendo uma busca no BD");
+		System.out.println("Processando os dados");
+		System.out.println("Exibindo os dados");
+	}
+	
+	public void executar(String param) {
+		System.out.println("Fazendo uma busca no BD com parametro" + param);
+		System.out.println("Processando os dados");
+		System.out.println("Exibindo os dados");
+	}
+	
+	public String executarRetorno(String param) {
+		return "Quem eh o Lindão? " +param;
+	}
+	
+	public String irParaIndex2() {
+		System.out.println(" entrou");
+		return "index2?faces-redirect=true";
+	}
+	
+	public void exibirNota() {
+		this.mostrarNota = true;
+		
+	}
+	
+	public void esconderNota() {
+		this.mostrarNota = false;
+	}
+	
+	public void exibirLink() {
+		this.mostrarLink = true;
+		
+	}
+	
+	public void esconderLink() {
+		this.mostrarLink = false;
+	}
 
 	
-	
-	
+	public boolean isMostrarNota() {
+		return mostrarNota;
+	}
+
+	public void setMostrarNota(boolean mostrarNota) {
+		this.mostrarNota = mostrarNota;
+	}
+
 	public Set<String> getNomeSet() {
 		return nomeSet;
 	}
@@ -49,6 +111,20 @@ public class EstudanteRegistroBean implements Serializable {
 	}
 	public void setEstudante(Estudante estudante) {
 		this.estudante = estudante;
+	}
+	public Map<String,String> getNomeMap() {
+		return nomeMap;
+	}
+	public void setNomeMap(Map<String,String> nomeMap) {
+		this.nomeMap = nomeMap;
+	}
+
+	public boolean isMostrarLink() {
+		return mostrarLink;
+	}
+
+	public void setMostrarLink(boolean mostrarLink) {
+		this.mostrarLink = mostrarLink;
 	}
 	
 	
