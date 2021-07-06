960d6b0b7ad8d91b3623430f8d576096ade84a73
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-07-06 04:34:44 -0300

    JSF

--- maratona/src/main/java/br/com/devdojo/maratonajsf/bean/session/TesteSessionBean.java
+++ maratona/src/main/java/br/com/devdojo/maratonajsf/bean/session/TesteSessionBean.java
@@ -1,4 +1,4 @@
-package br.com.devdojo.maratonajsf.bean.request;
+package br.com.devdojo.maratonajsf.bean.session;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -7,28 +7,32 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 @Named
-@RequestScoped
-public class TesteRequestBean implements Serializable{
+@SessionScoped
+//Session Scoped vai continuar mostrando até a janela do navegador não ser fechada
+public class TesteSessionBean implements Serializable  {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private List<String> personagens;
 	private List<String> personagemSelecionado = new ArrayList<>();
 	@PostConstruct
 	public void init() {
-		System.out.println("Entrou no PostConstruct do RequestScoped");
-		 personagens = Arrays.asList("Marvel","DC","Campcom");
+		System.out.println("Entrou no PostConstruct do SessionScoped");
+		 personagens = Arrays.asList("Monster","Red Bull","Reign");
 	}
+	
 	public void selecionaPersonagem() {
 		int index = ThreadLocalRandom.current().nextInt(3);
 		String personagem = personagens.get(index);
 		personagemSelecionado.add(personagem);
+	}
+	public String logout() {
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+		return "session?faces-redirect=true";
 	}
 
 	public List<String> getPersonagemSelecionado() {
@@ -38,9 +42,4 @@ public class TesteRequestBean implements Serializable{
 	public void setPersonagemSelecionado(List<String> personagemSelecionado) {
 		this.personagemSelecionado = personagemSelecionado;
 	}
-	
-	
-	
-	
-	
 }
