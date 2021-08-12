bfc5cdf855c7eb3e454c7ddabc033a5575ab84b8
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-08-11 22:28:46 -0300

    JSF

--- maratona/src/main/java/br/com/devdojo/maratonajsf/bean/comunicacao/ComunicacaoTeste12Bean.java
+++ maratona/src/main/java/br/com/devdojo/maratonajsf/bean/comunicacao/ComunicacaoTeste12Bean.java
@@ -21,10 +21,17 @@ public class ComunicacaoTeste12Bean implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	private Estudante estudante;
+	private Estudante estudante2 = new Estudante();
 	
 	public void init() {
 		System.out.println("entrou");
 		estudante = (Estudante) ApplicationMapUtil.getValueFromApplicationMap("estudante");
+	}
+	
+	public String voltar() {
+		System.out.println(estudante2.getNome());
+		System.out.println(estudante2.getSobrenome());
+		return "comunicacao11?faces-redirect=true";
 	}
 
 	public Estudante getEstudante() {
@@ -33,6 +40,14 @@ public class ComunicacaoTeste12Bean implements Serializable {
 
 	public void setEstudante(Estudante estudante) {
 		this.estudante = estudante;
+	}
+
+	public Estudante getEstudante2() {
+		return estudante2;
+	}
+
+	public void setEstudante2(Estudante estudante2) {
+		this.estudante2 = estudante2;
 	}
 
 	
