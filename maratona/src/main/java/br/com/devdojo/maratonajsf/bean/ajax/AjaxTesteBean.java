7d3b83e823e68e05ee4ae5d8ecfe0b5a604bd022
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-08-21 21:51:55 -0300

    JSF

--- maratona/src/main/java/br/com/devdojo/maratonajsf/bean/ajax/AjaxTesteBean.java
+++ maratona/src/main/java/br/com/devdojo/maratonajsf/bean/ajax/AjaxTesteBean.java
@@ -1,10 +1,14 @@
 package br.com.devdojo.maratonajsf.bean.ajax;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import javax.faces.event.AjaxBehaviorEvent;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import static java.util.Arrays.asList;
 
 @Named
 @ViewScoped
@@ -15,6 +19,17 @@ public class AjaxTesteBean implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private String nome;
 	private String sobrenome;
+	private String email;
+	private Map<String, List<String>> animePersonagensMap;
+	private List<String> personagens;
+	private String animeSelecionado;
+	private String personagemSelecionado;
+	{
+		animePersonagensMap = new TreeMap<>();
+		animePersonagensMap.put("Senhor dos Anéis", asList("Gandalf","Gollum", "Frodo"));
+		animePersonagensMap.put("Corrida", asList("Need For Speed","Formula 1", "Gran Turismo"));
+		animePersonagensMap.put("Atletas", asList("Michael Phelps","Lebron James", "Mineirinho"));
+	}
 	
 	public void toUpperCase(AjaxBehaviorEvent e) {
 		this.nome = this.nome.toUpperCase();
@@ -23,11 +38,17 @@ public class AjaxTesteBean implements Serializable{
 		System.out.println(sobrenome);
 	}
 	
-	public void toUpperCase2 () {
+	public void onSelectAnime() {
+		if(animeSelecionado == null) {
+			personagens = null;
+			return;
+		}
+		personagens = animePersonagensMap.get(animeSelecionado);
+	}
+
+	public void toUpperCaseNome () {
 		this.nome = this.nome.toUpperCase();
-		this.sobrenome = this.sobrenome.toUpperCase();
-		System.out.println(nome);
-		System.out.println(sobrenome);
+		
 	}
 	public String getNome() {
 		return nome;
@@ -40,6 +61,46 @@ public class AjaxTesteBean implements Serializable{
 	}
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
+	}
+
+	public Map<String, List<String>> getAnimePersonagensMap() {
+		return animePersonagensMap;
+	}
+
+	public void setAnimePersonagensMap(Map<String, List<String>> animePersonagensMap) {
+		this.animePersonagensMap = animePersonagensMap;
+	}
+
+	public List<String> getPersonagens() {
+		return personagens;
+	}
+
+	public void setPersonagens(List<String> personagens) {
+		this.personagens = personagens;
+	}
+
+	public String getAnimeSelecionado() {
+		return animeSelecionado;
+	}
+
+	public void setAnimeSelecionado(String animeSelecionado) {
+		this.animeSelecionado = animeSelecionado;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPersonagemSelecionado() {
+		return personagemSelecionado;
+	}
+
+	public void setPersonagemSelecionado(String personagemSelecionado) {
+		this.personagemSelecionado = personagemSelecionado;
 	}
 	
 	
